d86d2d48796ae2859717005a22ecf9ff9c20f2ab
Robert Melton <rsmelton@crimson.ua.edu>
2024-12-28 00:56:15 -0600

    Testing..

--- api/cart.js
+++ api/cart.js
@@ -8,19 +8,21 @@ import Item from "../models/item.model"
 
 module.exports = async (req, res) => {
 
-    res.setHeader('Access-Control-Allow-Origin', '*');  // Allow all origins (be more specific in production)
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed HTTP methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allowed headers
-    
-    // Handle preflight OPTIONS request (CORS preflight)
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();  // Respond immediately to preflight request
-      return;
-    }
+    res.status(200).json( {message: "We are getting the cart items successfully!"} );
 
-    if (req.method === 'GET') {
-        res.status(200).json( {message: "We are getting the cart items successfully!"} );
-    }
+    // res.setHeader('Access-Control-Allow-Origin', '*');  // Allow all origins (be more specific in production)
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed HTTP methods
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allowed headers
+    
+    // // Handle preflight OPTIONS request (CORS preflight)
+    // if (req.method === 'OPTIONS') {
+    //   res.status(200).end();  // Respond immediately to preflight request
+    //   return;
+    // }
+
+    // if (req.method === 'GET') {
+    //     res.status(200).json( {message: "We are getting the cart items successfully!"} );
+    // }
 }
 
 // export const getItems = async (req, res) => {
