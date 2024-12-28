b21c074e0491d60be3d027749b353d2827e0072f
Robert Melton <rsmelton@crimson.ua.edu>
2024-12-28 01:29:13 -0600

    Changed vercel json file again

--- api/cart.js
+++ api/cart.js
@@ -2,28 +2,28 @@ import express from 'express'
 import mongoose from "mongoose"
 import Item from "../models/item.model"
 
-// app.get('https://dragons-brew-cafe-serverless-apis.vercel.app/api/cart', async (req, res) => {
-//     res.status(200).json( {message: "We are getting the cart items successfully!"} );
-// });
-
-module.exports = async (req, res) => {
-
+app.get('/api/cart', async (req, res) => {
     res.status(200).json( {message: "We are getting the cart items successfully!"} );
+});
 
-    // res.setHeader('Access-Control-Allow-Origin', '*');  // Allow all origins (be more specific in production)
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed HTTP methods
-    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allowed headers
+// module.exports = async (req, res) => {
+
+//     res.status(200).json( {message: "We are getting the cart items successfully!"} );
+
+//     res.setHeader('Access-Control-Allow-Origin', '*');  // Allow all origins (be more specific in production)
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed HTTP methods
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allowed headers
     
-    // // Handle preflight OPTIONS request (CORS preflight)
-    // if (req.method === 'OPTIONS') {
-    //   res.status(200).end();  // Respond immediately to preflight request
-    //   return;
-    // }
+//     // Handle preflight OPTIONS request (CORS preflight)
+//     if (req.method === 'OPTIONS') {
+//       res.status(200).end();  // Respond immediately to preflight request
+//       return;
+//     }
 
-    // if (req.method === 'GET') {
-    //     res.status(200).json( {message: "We are getting the cart items successfully!"} );
-    // }
-}
+//     if (req.method === 'GET') {
+//         res.status(200).json( {message: "We are getting the cart items successfully!"} );
+//     }
+// }
 
 // export const getItems = async (req, res) => {
 //     console.log("Cart GET API entered")
