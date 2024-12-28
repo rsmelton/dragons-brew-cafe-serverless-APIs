f6607272afd7707795520f51b089a181e8175cd7
Robert Melton <rsmelton@crimson.ua.edu>
2024-12-28 00:20:07 -0600

    Installed express and other changes

--- api/cart.js
+++ api/cart.js
@@ -1,18 +1,26 @@
+import express from 'express'
 import mongoose from "mongoose"
 import Item from "../models/item.model"
 
-export const getItems = async (req, res) => {
-    console.log("Cart GET API entered")
-    try {
-        // this grabs all Item objects from the database
-        const items = await Item.find({})
+app.get('https://dragons-brew-cafe-serverless-apis.vercel.app/api/cart', async (req, res) => {
+    res.send("We are getting the cart items successfully!");
+});
 
-        // **** newly added code ****
-        if (items === null) res.status(204).json({ success: true, data: items})
+module.exports = app
 
-        res.status(200).json({ success: true, data: items })
-    } catch (error) {
-        console.log("Error in fetching cart items:", error.message)
-        res.status(500).json({ success: false, message: "Server Error" })
-    }
-}
+// export const getItems = async (req, res) => {
+//     console.log("Cart GET API entered")
+//     res.send("We are getting the cart items successfully!");
+//     try {
+//         // this grabs all Item objects from the database
+//         const items = await Item.find({})
+
+//         // **** newly added code ****
+//         if (items === null) res.status(204).json({ success: true, data: items})
+
+//         res.status(200).json({ success: true, data: items })
+//     } catch (error) {
+//         console.log("Error in fetching cart items:", error.message)
+//         res.status(500).json({ success: false, message: "Server Error" })
+//     }
+// }
